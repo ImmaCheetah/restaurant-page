8c0d957b88da72d9c57bb0d18a03213c2bb571ca
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-11-10 16:43:49 -0500

    Add and link css

--- src/home.js
+++ src/home.js
@@ -5,6 +5,13 @@ export default function homePage() {
     const info = document.createElement('p');
     const content = document.getElementById('content');
 
-    
+    header.textContent = 'Welcome to this restaurant';
+    headerDiv.appendChild(header);
+
+    info.textContent = 'Stuff about this restaurant';
+    infoDiv.appendChild(info);
+
+    content.appendChild(headerDiv);
+    content.appendChild(infoDiv);
     console.log("home page console log");
 }
