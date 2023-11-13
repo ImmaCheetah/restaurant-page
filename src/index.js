f2c8bfaf7dea0bdfa6afa0ea2830e1cda457e7df
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-11-13 17:57:37 -0500

    Create buttons for each page and append to content div

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import homePage from './home.js';
 import menuPage from './menu.js';
 import contactPage from './contact.js';
 
+const navBarDiv = document.createElement('div');
+const homeBtn = document.createElement('button');
+const menuBtn = document.createElement('button');
+const contactBtn = document.createElement('button');
+const content = document.getElementById('content');
+
+homeBtn.textContent = 'HOME';
+menuBtn.textContent = 'MENU';
+contactBtn.textContent = 'CONTACT';
+
+navBarDiv.appendChild(homeBtn);
+navBarDiv.appendChild(menuBtn);
+navBarDiv.appendChild(contactBtn);
+
+content.appendChild(navBarDiv);
+
 console.log('oi fam');
 // homePage();
 // menuPage();
